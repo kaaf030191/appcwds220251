bf4ff3f7cd561403b1cd2c99abe1c20d7ab5b663
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2025-05-22 19:49:30 -0500

    Feat: 22-05-2025 - Implementación de Bootstrap.
    Framework Bootstrap implementado en el proyecto.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'appcwds20251' title`, () => {
+  it(`should have the 'appcwds220251' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('appcwds20251');
+    expect(app.title).toEqual('appcwds220251');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, appcwds20251');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, appcwds220251');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	providers: [
+		BsModalService
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'appcwds20251';
+	constructor(
+		private modalService: BsModalService
+	) {}
+
+	showModal(myModal: TemplateRef<any>): void {
+		this.modalService.show(myModal);
+	}
+
+	closeModal(): void {
+		this.modalService.hide();
+	}
 }
