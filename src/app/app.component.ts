80608b4208265e2a5eb7d7468fdeb4eaf47746fe
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2025-05-29 07:18:14 -0500

    Feat: 27-05-2025 - Diseño de formulario de registro.
    Diseño de formulario con Bootstrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component, TemplateRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
 	selector: 'app-root',
 	imports: [RouterOutlet],
-	providers: [
-		BsModalService
-	],
+	providers: [],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css'
 })
 export class AppComponent {
-	constructor(
-		private modalService: BsModalService
-	) {}
-
-	showModal(myModal: TemplateRef<any>): void {
-		this.modalService.show(myModal);
-	}
-
-	closeModal(): void {
-		this.modalService.hide();
-	}
+	constructor() {}
 }
---
+++ src/app/page/person/person-insert/person-insert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-insert',
+  imports: [],
+  templateUrl: './person-insert.component.html',
+  styleUrl: './person-insert.component.css'
+})
+export class PersonInsertComponent {
+
+}
